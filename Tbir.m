c4aec0ddb18851f988e4b128f988881f5390d01f
SirnamSwetha <sirnam.swetha@gmail.com>
2014-10-25 02:42:25 +0530

    Updated TBIR

--- Tbir.m
+++ Tbir.m
@@ -1,8 +1,9 @@
-% Process the query Image
+%% Process the query Image
+
 % Assume user gives the tags
+Tags = [ 6 ];
 
-Tags = [30];
-
+ImageIndex = 986;
 %% Read Query Image
 
 FID = fopen('result_file_Try4/TryNewImage');
@@ -50,11 +51,14 @@ IDX = knnsearch(D,B);
 
 %% generating image_clust
 
+image_cluster = [];
+
 for j = 1:1:500
     image_cluster(j) = 0;
 end
 
 f1id = fopen('result_file_Try4/q_words.txt','w');
+
 for j = 1:1:size
         if IDX(j) > 0
                 image_cluster(IDX(j)) = image_cluster(IDX(j))+1;
@@ -64,72 +68,76 @@ end
 
 query_image_sparse = sparse(image_cluster);
 
-
-
-
-
 %% Data
 
 fall = fopen('data_Try4.txt');
 All = fscanf(fall,'%g');
 
-fReq = fopen('outputmat.txt');
-Req = fscanf(fReq,'%g');
+fReq = fopen('Outputmat.txt');
+Req = fscanf(fReq,'%u');
 
 tot = 5000;
 
-TagMatrix = zeros(5000,31);
+% Build the Tag Matrix
+TagMatrix = [];
 
 for i = 1:1:tot
 	tmp = [];
-	for j = 1:1:30
+	for j = 1:1:31
 		tmp = [ tmp Req( j + (i-1)*31 )];
 	end
 	TagMatrix = [TagMatrix ; tmp ];
 end
 
+%% Test
 
-
+Test = [];
 Image_hists = [];
+% search space size is search_size
+search_size = 0;
+
 for i = 1:1:tot
-        tmp = [];
-		% flag to check if the image has all the tags specified by user
-		Tagflag = 0;
-		% check if ith image has the req tags
-		for index = 1:1:numel(size(Tags))
-			if TagMatrix(i,index) == 1
-				continue;
-			else
-				Tagflag = 1; 
-		end
-		if Tagflag == 0
-	        for j = 1:1:500
-    	            tmp = [ tmp All( j + (i-1)*500 ) ];
-    	    end
-    	    Image_hists = [ Image_hists ; tmp ];
-		end
+    Tagflag = 0;
+    tmp = [];
+    for index = 1:1:numel(Tags)
+        if TagMatrix(i,index) ~= TagMatrix(ImageIndex,index)
+            Tagflag = 1;
+        end
+    end
+    if Tagflag == 0
+        for j = 1:1:500
+    	    tmp = [ tmp All( j + (i-1)*500 ) ];
+    	end
+    	Image_hists = [ Image_hists ; tmp ];
+
+        Test = [ Test; i];
+        search_size = search_size + 1;
+%       TagMatrix(i,:) 
+    end
 end
 
-InvertedSparseMatrix = sparse(Image_hists);
-disp(' Sparse')
-%idfSparse
-idfSparse = [];
+
+%% Final Edit
+
+idfSparse = []; % size:500
+
 for i = 1 :1:500
 	idfSparse(i) = 0;
 end
 
-[i, j, s] = find(InvertedSparseMatrix);
 
-%for [Img, Clust, count] in [i, j, s]:
-for el = 1:1:numel(i)
-	%find idfi
-	idfSparse(j(el)) = idfSparse(j(el)) + 1;	
+for image = 1:1:search_size
+   for col = 1:1:500
+       if Image_hists(image,col) > 0
+            idfSparse(col) = idfSparse(col) + 1;
+       end
+   end
 end
 
-%idfSparse
+
+% idf sparse
 
 idf = [];
-disp('done idf ')
 for val = 1:1:500
 	%compute idf
 	if idfSparse(val) > 0
@@ -139,41 +147,27 @@ for val = 1:1:500
 	end
 end
 
-%idf
-disp('really!! ')
-%tf_idf = zeros(size(Image_hists))
+% tf_idf [search_sizex500]
 
 tf_idf = [];
-for row = 1:1:766
+for row = 1:1:search_size
 	for col = 1:1:500
-		tf_idf(row,col) = 0;
+		tf_idf(row,col) = Image_hists(row,col)*idf(col);
 	end
 end
 
-%for [ i1, j1, s1 ] in [i, j, s]:
-for el = 1:1:numel(i)
-	%tf(i(el),j(el))
-	tf_idf(i(el),j(el)) = s(el)*idf(j(el));
-end
-
-%%transpose
-%tf_idf = transpose(tf_idf);
-Sparsetf_idf = sparse(tf_idf);
-disp('end')
-
-
 %% Now find tf-idf for the query image
-[irow, icol, scount] = find(query_image_sparse);
+ [irow, icol, scount] = find(query_image_sparse);
 
 for el = 1:1:numel(irow)
         %find idfi
-        idfSparse(icol(el)) = idfSparse(icol(el)) + 1;
+        idfSparse(icol(el)) = idfSparse(icol(el)) + 1; % After sparse
 end
 
 %% idfSparse
 
 qidf = [];
-disp('Q done idf ')
+
 for val = 1:1:500
         %compute idf
         if idfSparse(val) > 0
@@ -184,8 +178,6 @@ for val = 1:1:500
 end
 
 %idf
-disp('Q really!! ')
-
 qtf_idf = [];
 
 for col = 1:1:500
@@ -198,10 +190,11 @@ for el = 1:1:numel(irow)
 end
 
 Sparseqtf_idf = sparse(qtf_idf);
+Sparsetf_idf = sparse(tf_idf);
 
 image_scores = [];
-for i = 1:1:tot
-        image_scores(i) = 0;
+for iter = 1:1:tot
+        image_scores(iter) = 0;
 end
 
 [ img, clust, count ] = find(Sparseqtf_idf);%query_image_sparse);
@@ -211,36 +204,32 @@ end
 totalValues = nnz(Sparsetf_idf);%InvertedSparseMatrix);
 
 for vw = 1:1:numel(img)
-        
         visualWord = clust(vw);
         for tr = 1:1:totalValues
                 if Cluster(tr) == visualWord
                         %increment count
-                       	image_scores(Image(tr)) = image_scores(Image(tr)) + count(vw);% + Counter(tr);
+                       	image_scores(Test(Image(tr))) = image_scores(Test(Image(tr))) +  Counter(tr);
                 end
                 if Cluster(tr) > visualWord
                         break
-                %else
-                %       continue
                 end
         end
 
 end
 
-disp('Done!! :)')
+% disp('Done!! :)')
 
 [ img_score Index ] = sort(image_scores,'descend');
 
-img_score(1:70)
-Index(1:70)
+img_score(1:40)
+Index(1:40)
 
-%delete '/home/swetha/Documents/result.txt';
-img_path = '';
-fid = fopen('result_file_Try4/result.txt','w');
-%fid = fopen('result.txt','w');
-for i=1:70
-% img_path=sprintf('Dataset_4/myntra_%03d.jpg',Index(i));
-img_path=sprintf('myntra_%d.jpg',Index(i));
-fprintf(fid,[img_path,'\n']);
-% figure, imshow(imread(img_path));
-end
+% img_path = '';
+% fid = fopen('result_file_Try4/result.txt','w');
+% %fid = fopen('result.txt','w');
+% for iter=1:70
+% % img_path=sprintf('Dataset_4/myntra_%03d.jpg',Index(i));
+% img_path=sprintf('myntra_%d.jpg',Index(iter));
+% fprintf(fid,[img_path,'\n']);
+% % figure, imshow(imread(img_path));
+% end
